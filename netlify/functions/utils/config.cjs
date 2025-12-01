5260fbbfdf237133dcb08a26c501a37496aee363
Martinsschnee <martinsschneevongestern@outlook.de>
2025-12-01 23:41:06 +0100

    reworked

--- netlify/functions/utils/config.cjs
+++ netlify/functions/utils/config.cjs
@@ -1,5 +1,5 @@
 // This file contains the hardcoded Site ID for Netlify Blobs.
 // You must update this value after creating your site on Netlify.
-const SITE_ID = '7946bca6-ec2d-477c-bd17-31af442dc105';
+const SITE_ID = '43b0cca0-f503-445a-bc5c-b29d0a49df0e';
 
 module.exports = { SITE_ID };
